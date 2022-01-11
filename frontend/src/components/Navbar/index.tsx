b3a14cfd4c08a37a952b25e7eb1a245c3382911b
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2022-01-11 09:17:55 -0300

    Pagination layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,21 +1,21 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg'
-import './styless.css'
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styless.css";
 
-function Navbar(){
-    return(
-        <header>
-        <nav className="container">
-          <div className="dsmovie-nav-content">
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-            <a href="https://github.com/alexsandro-cristiano">
-              <div className="dsmovie-contact-container">
-               < GithubIcon/>
-                <p className="dsmovie-contact-link">/alexsandro-cristiano</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
-    );
+          <a href="https://github.com/alexsandro-cristiano">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/alexsandro-cristiano</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 export default Navbar;
