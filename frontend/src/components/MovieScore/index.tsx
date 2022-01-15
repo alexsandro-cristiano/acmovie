d18f3997c7a4c6834d5043bd47e0020c3d6766db
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2022-01-15 15:27:16 -0300

    show core

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,16 +1,18 @@
 import MovieStars from "components/MovieStars";
 import "./styless.css";
 
-function MovieScore() {
-  let score = 2;
-  let count = 100;
+type Props = {
+  score: number;
+  count: number;
+};
 
+function MovieScore({ score, count } : Props) {
   return (
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">
         {score > 0 ? score.toFixed(1) : "-"}
       </p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
